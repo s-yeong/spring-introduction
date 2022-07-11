f7b4acbcd4168b61ebfd64f821f0aaeca58d9522
s-yeong <syxxng25@gmail.com>
2022-07-11 20:35:31 +0900

    스프링 웹 개발 기초

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -12,5 +14,43 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "spring~!");
         return "hello"; }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    // ResponeBody의 의미는 HTTP에서 Body에 이 데이터를 직접 넣어주겠다. (문자 내용 직접 반환)
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+        // "hello spring" 이 문자가 요청한 클리언트에게 그대로 데려감
+    }
+
+    // API 방식
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        // Hello 객체 만듬, static으로 만들면 HelloController 안에 이 것을 또 쓸 수있음
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+
 
 }
