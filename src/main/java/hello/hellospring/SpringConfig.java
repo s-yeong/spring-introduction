0d8e2560117bf3e466d12a1408c4bb66567164c3
s-yeong <syxxng25@gmail.com>
2022-07-15 19:52:06 +0900

    스프링 DB 접근 기술 - 순수 JDBC

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,14 +1,24 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberCodeRepository;
 import hello.hellospring.service.MemberCodeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 // 스프링이 뜰 때 Configuration읽고 이 것을 스프링 빈에 등록하라는 뜻이네 라고 암
+
+    private DataSource dataSource;
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberCodeService memberCodeService(){
         return new MemberCodeService(memberRepository());
@@ -16,7 +26,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberCodeRepository();
+        //return new MemoryMemberCodeRepository();
         // 인터페이스는 new 안됨, 구현체를 넣는거임
+        return new JdbcMemberRepository(dataSource);
     }
 }
--- src/main/java/hello/hellospring/controller/HomeController.java
+++ src/main/java/hello/hellospring/controller/HomeController.java
@@ -1,9 +1,7 @@
 package hello.hellospring.controller;
 
-import hello.hellospring.domain.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
